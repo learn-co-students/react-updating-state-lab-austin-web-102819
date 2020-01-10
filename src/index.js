4e947dc57abe7335a16806d484dafd8077f96184
Heidi Boulter <boulter.heidi@gmail.com>
2020-01-09 22:27:03 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import DigitalClicker from './components/DigitalClicker';
-import YouTubeDebugger from './components/YouTubeDebugger';
+import DigitalClicker from "./components/DigitalClicker";
+// eslint-disable-next-line
+import YouTubeDebugger from "./components/YouTubeDebugger";
 
 ReactDOM.render(
   <div>
     <DigitalClicker />
     <YouTubeDebugger />
   </div>,
-  document.getElementById('global')
+  document.getElementById("global")
 );
